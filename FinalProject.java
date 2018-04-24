73d3ef7136a57335b07c63bd5b1c53a4c89c52ed
Jnissenbaum01 <35780095+Jnissenbaum01@users.noreply.github.com>
2018-04-24 19:11:32 -0400

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,12 +1,27 @@
-// import a bunch of stuff
+import javax.swing.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+
+//delete whatever we don't end up using
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
 * Final Project for COSI 12b Spring 2018
+* We are going to make a Address Book.
+* You can enter a name, their email address, their phone number and it will be saved.
+* You can search for a name, and it will show you their email address and phone number.
 **/
 public class FinalProject
 {
 	Map<String, Entry> addressBook;
 	
+	//static references to certain GUI things
 	
 	public static void main(String[] args)
 	{
@@ -17,6 +32,16 @@ public class FinalProject
 	public static void CreateGUI()
 	{
 		//fill this out
+	}
+	
+	public static Map<String,Entry> readToFile()
+	{
+		// fill this out
+	}
+	
+	public static Map<String,Entry> writeToFile()
+	{
+		// fill this out
 	}
 	
 	public static class Entry
