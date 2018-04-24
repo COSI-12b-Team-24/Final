b3beb075116d39be874c6026cd54b1a23e3311de
Jnissenbaum01 <35780095+Jnissenbaum01@users.noreply.github.com>
2018-04-24 19:08:45 -0400

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,13 +1,26 @@
+// import a bunch of stuff
+
 /**
 * Final Project for COSI 12b Spring 2018
 **/
 public class FinalProject
 {
-
+	Map<String, Entry> addressBook;
+	
+	
 	public static void main(String[] args)
 	{
-	
+		//fill this out
+		CreateGUI();
 	}
 	
+	public static void CreateGUI()
+	{
+		//fill this out
+	}
 	
+	public static class Entry
+	{
+		// fill this out
+	}
 }
